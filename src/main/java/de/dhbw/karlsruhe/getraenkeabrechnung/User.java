7a1cd7ac17d79fab30b7559164c677a20c4e0536
Fichtentee <ficht.sarah@yahoo.de>
2025-03-03 17:04:53 +0100

    refactor: :construction: adapted user class to fit to changes made for password and username validation

--- src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/User.java
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/User.java
@@ -1,9 +1,32 @@
 package de.dhbw.karlsruhe.getraenkeabrechnung;
 
+    // Todo 
+    // 1. login method (Session etc.)
+    // 2. Klassen für Feld Passwort erstellen
+
 public class User {
     private String username;
     private String password;
+    private String realFirstName;
+    private String realLastName;
+    private String realName;
+    private String email;
+    private Konto konto;
+    private GetraenkeAutomat getraenkeAutomat;
 
+    public User(String username, String password, String realFirstName, String realLastName,
+        String realName, String email, Konto konto, GetraenkeAutomat getraenkeAutomat) {
+
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.realName = realFirstName + " " + realLastName;
+        this.getraenkeAutomat = new GetraenkeAutomat();
+        this.konto = new Konto();
+        konto.setAccountCredit(0);
+    }
+
+    // Constructor for class LoginCommand
     public User(String username, String password) {
         this.username = username;
         this.password = password;
@@ -12,4 +35,82 @@ public class User {
     public String getUsername() {
         return username;
     }
+
+    public void setUsername(String username) {
+        if (UsernameValidator.isValidUsername(username)) {
+            this.username = username;
+        } else {
+            throw new IllegalArgumentException("Username not valid!");
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (PasswordValidator.isValidPassword(password)) {
+            this.password = password;
+        } else {
+            throw new IllegalArgumentException("Password not valid!");
+        }
+    }
+
+    public String getRealFirstName() {
+        return realFirstName;
+    }
+
+    public void setRealFirstName(String realFirstName) {
+        this.realFirstName = realFirstName;
+    }
+
+    public String getRealLastName() {
+        return realLastName;
+    }
+
+    public void setRealLastName(String realLastName) {
+        this.realLastName = realLastName;
+    }
+
+    public String getRealName() {
+        realName = realFirstName + " " + realLastName;
+        return realName;
+    }
+
+        public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Konto getKonto() {
+        return konto;
+    }
+
+    public void setKonto(Konto konto) {
+        this.konto = konto;
+    }
+
+    public GetraenkeAutomat getGetraenkeAutomat() {
+        return getraenkeAutomat;
+    }
+
+    public void setGetraenkeAutomat(GetraenkeAutomat getraenkeAutomat) {
+        this.getraenkeAutomat = getraenkeAutomat;
+    }
+
+    public void addCreditAmount(double credit) {
+        konto.addAccountCredit(credit);
+    }
+
+    public void removeCreditAmount(double credit) {
+        konto.removeAccountCredit(credit);
+    }
+
+    public double getAccountCredit() {
+        return konto.getAccountCredit();
+    }
+
 }
