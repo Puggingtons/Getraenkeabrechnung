ceace5a0e2c156b33cbef2331b67999872f66259
Fichtentee <ficht.sarah@yahoo.de>
2025-03-05 11:48:18 +0100

    feat: :sparkles: use password model

---
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/validators/PasswordValidator.java
@@ -0,0 +1,44 @@
+package de.dhbw.karlsruhe.getraenkeabrechnung.validators;
+
+import java.util.Optional;
+
+import de.dhbw.karlsruhe.getraenkeabrechnung.Password;
+
+public class PasswordValidator {
+
+    // credits: https://stackoverflow.com/questions/3802192/regexp-java-for-password-validation
+
+    // ^                 # start-of-string
+    // (?=.*[0-9])       # a digit must occur at least once
+    // (?=.*[a-z])       # a lower case letter must occur at least once
+    // (?=.*[A-Z])       # an upper case letter must occur at least once
+    // (?=.*[@#$%^&+=])  # a special character must occur at least once
+    // (?=\S+$)          # no whitespace allowed in the entire string
+    // .{8,}             # anything, at least eight places though
+    // $                 # end-of-string
+
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+    public static boolean isValidPassword(Password password) {
+
+        boolean validPassword = password.isValid(Optional.of(PASSWORD_PATTERN));
+
+        if (!validPassword) {
+            System.out.println("Password does not match pattern! The password must be 8 characters long, contain a number, an upper and lower case letter and a special character (@#$%^&+=).");
+        }
+
+        return validPassword;
+    }
+
+    // Test policy of username according to regex rules
+    public static void main(String[] args) {
+        String[] testPasswords = {
+            "Short3@", "no!SpeChar1", "NOLOWER#1", "noupper#2", "noNumber#", "goodPassword=1"
+        };
+
+        for (String password : testPasswords) {
+            System.out.println("Test: " +  password + " → " + isValidPassword(new Password(password)));
+        }
+    }
+
+}
