a0b62dd8a59c875ea223edecf1859725840faac9
Paul Bader <paul@pbdr.de>
2025-03-11 23:30:09 +0100

    feat: add SelectInput

---
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/io2/SelectInput.java
@@ -0,0 +1,25 @@
+package de.dhbw.karlsruhe.getraenkeabrechnung.io2;
+
+public class SelectInput extends Input {
+
+    private final String[] options;
+
+    public SelectInput(String[] options) {
+        super();
+        this.options = options;
+    }
+
+    @Override
+    public String prompt() {
+        printOptions();
+        return readInput();
+    }
+
+    private void printOptions() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < options.length; i++) {
+            builder.append("[").append(i).append("] ").append(options[i]).append("\n");
+        }
+        print(builder.toString());
+    }
+}
---
+++ src/test/java/io2/SelectInputTest.java
@@ -0,0 +1,50 @@
+package io2;
+
+import de.dhbw.karlsruhe.getraenkeabrechnung.io2.SelectInput;
+import io2.mocks.InputReaderMock;
+import io2.mocks.OutputWriterMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SelectInputTest {
+    private InputReaderMock readerMock;
+    private OutputWriterMock writerMock;
+
+    @BeforeEach
+    public void setup() {
+        readerMock = new InputReaderMock();
+        writerMock = new OutputWriterMock();
+    }
+
+    @Test
+    public void itPrintsOption() {
+        String[] options = new String[]{"Prompt"};
+        SelectInput input = new SelectInput(options);
+
+        input.setReader(readerMock);
+        input.setWriter(writerMock);
+
+        input.prompt();
+
+        assertEquals("[0] " + options[0] + "\n", writerMock.getOutput());
+    }
+
+    @Test
+    public void itPrintsAllOptions() {
+        String[] options = new String[]{"Prompt", "Prompt2", "Prompt3"};
+        SelectInput input = new SelectInput(options);
+
+        input.setReader(readerMock);
+        input.setWriter(writerMock);
+
+        input.prompt();
+
+        String expectedOutput = "[0] " + options[0] + "\n[1] " + options[1] + "\n[2] " + options[2] + "\n";
+
+        assertEquals(expectedOutput, writerMock.getOutput());
+    }
+
+//    todo: test unhappy paths
+}
