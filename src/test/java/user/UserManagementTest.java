f6b0a969f521c84efd65f1073a26993df031cbcc
Kayra Güler <kayra.gueler32@gmail.com>
2025-03-12 09:57:41 +0100

    feat: Prepared more tests for user management

--- src/test/java/user/UserManagementTest.java
+++ src/test/java/user/UserManagementTest.java
@@ -10,21 +10,39 @@ class UserManagementTest {
 
     @Test
     void testValidUserCreation() {
-        User user = new User(new Username(null), new Password(null));
-        user.setUsername(new Username("valid_User123"));
-        user.setPassword(new Password("goodPassword=1"));
+        User user = new User(new Username("valid_User123"), new Password("goodPassword=1"));
         assertNotNull(user);
     }
 
+    // Test will get implemented when validators are also implemented for User creation
+    /* @Test
+    void testInvalidUserCreation() {
+        User user = new User(new Username("_invalidUser"), new Password("badPassword"));
+        assertNull(user);
+    } */
+
     @Test
-    void testInvalidUserCreationWithEmptyUsername() {
+    void testSetInvalidUsername() {
         User user = new User(new Username(null), new Password("goodPassword=1"));
-        assertThrows(NullPointerException.class, () -> user.setUsername(new Username(null)));
+        assertThrows(IllegalArgumentException.class, () -> user.setUsername(new Username("")));
     }
 
     @Test
-    void testInvalidUserCreationWithEmptyPassword() {
+    void testSetInvalidPassword() {
         User user = new User(new Username("valid_User123"), new Password(null));
-        assertThrows(NullPointerException.class, () -> user.setPassword(new Password(null)));
+        assertThrows(IllegalArgumentException.class, () -> user.setPassword(new Password("")));
     }
+
+    // Tests will get implemented when a toString method is implemented for User
+    /* @Test
+    void testGetUsername() {
+        User user = new User(new Username("valid_User123"), new Password("goodPassword=1"));
+        assertEquals("valid_User123", user.getUsername());
+    }
+
+    @Test
+    void testGetPassword() {
+        User user = new User(new Username("valid_User123"), new Password("goodPassword=1"));
+        assertEquals("goodPassword=1", user.getPassword());
+    } */
 }
