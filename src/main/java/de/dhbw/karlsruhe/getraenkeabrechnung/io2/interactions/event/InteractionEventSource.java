9f15e2ac46e6c51406f440603a02acb9ec06b783
Paul Bader <paul@pbdr.de>
2025-03-16 15:05:58 +0100

    feat: add InteractionEventSource

---
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/io2/interactions/event/InteractionEventSource.java
@@ -0,0 +1,34 @@
+package de.dhbw.karlsruhe.getraenkeabrechnung.io2.interactions.event;
+
+import java.util.function.Consumer;
+
+public class InteractionEventSource<T> {
+
+    private Consumer<T> onSuccess;
+    private Consumer<Void> onFailure;
+
+    public InteractionEventSource() {
+        this.onSuccess = null;
+        this.onFailure = null;
+    }
+
+    public void onSuccess(Consumer<T> onSuccess) {
+        this.onSuccess = onSuccess;
+    }
+
+    protected void success(T t) {
+        if (onSuccess != null) {
+            onSuccess.accept(t);
+        }
+    }
+
+    public void onFailure(Consumer<Void> onFailure) {
+        this.onFailure = onFailure;
+    }
+
+    protected void failure() {
+        if (onFailure != null) {
+            onFailure.accept(null);
+        }
+    }
+}
