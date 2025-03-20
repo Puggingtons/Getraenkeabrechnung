8aebc45cc10a8e14f48e313e47b4e41a03d33aa4
Paul Bader <paul@pbdr.de>
2025-03-20 17:34:51 +0100

    feat: add Saveable

---
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/data/Savable.java
@@ -0,0 +1,48 @@
+package de.dhbw.karlsruhe.getraenkeabrechnung.data;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Savable<T> {
+    private T t;
+
+    public Savable(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public void load(String path, TypeToken<T> typeToken) throws IOException {
+        load(Path.of(path), typeToken);
+    }
+
+    /**
+     * Loads the data from the file system.
+     * We need to provide a TypeToken so the parsing can be done correctly. See
+     * <a href="https://github.com/google/gson/blob/main/UserGuide.md#collections-examples">here</a>.
+     *
+     * @param path      the path to the file
+     * @param typeToken the type of the parsed data
+     * @throws IOException
+     */
+    public void load(Path path, TypeToken<T> typeToken) throws IOException {
+        Gson gson = new Gson();
+
+        t = gson.fromJson(Files.readString(path), typeToken.getType());
+    }
+
+    public void save(String path) throws IOException {
+        save(Path.of(path));
+    }
+
+    public void save(Path path) throws IOException {
+        Gson gson = new Gson();
+        Files.writeString(path, gson.toJson(t));
+    }
+}
--- src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/data/UserDatabase.java
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/data/UserDatabase.java
@@ -1,44 +1,41 @@
 package de.dhbw.karlsruhe.getraenkeabrechnung.data;
 
-import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import de.dhbw.karlsruhe.getraenkeabrechnung.User;
 import de.dhbw.karlsruhe.getraenkeabrechnung.Username;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDatabase {
-    private List<User> users;
-    private static final String PATH = "users.json";
+    private Savable<List<User>> users;
 
     public UserDatabase() {
-        this.users = new ArrayList<>();
+        this.users = new Savable<>(new ArrayList<>());
     }
 
     public User[] getUsers() {
-        return users.toArray(new User[0]);
+        return users.get().toArray(new User[0]);
     }
 
     public void addUser(User user) {
-        this.users.add(user);
+        this.users.get().add(user);
     }
 
     public void registerNewUser(User user) {
-        for (User u : users) {
+        for (User u : users.get()) {
             if (u.getUsername().equals(user.getUsername())) {
                 return;
             }
         }
 
-        users.add(user);
+        users.get().add(user);
     }
 
     public boolean userExists(Username username) {
-        for (User u : users) {
+        for (User u : users.get()) {
             if (u.getUsername().equals(username)) {
                 return true;
             }
@@ -47,33 +44,20 @@ public class UserDatabase {
         return false;
     }
 
-    public void load() throws IOException {
-        load(PATH);
-    }
 
     public void load(String path) throws IOException {
         load(Path.of(path));
     }
 
     public void load(Path path) throws IOException {
-        Gson gson = new Gson();
-
-        // https://github.com/google/gson/blob/main/UserGuide.md#collections-examples
-        TypeToken<List<User>> typeToken = new TypeToken<List<User>>() {};
-        this.users = gson.fromJson(Files.readString(path), typeToken.getType());
-    }
-
-    public void save() throws IOException {
-        save(PATH);
+        users.load(path, new TypeToken<List<User>>() {});
     }
 
     public void save(String path) throws IOException {
-        save(Path.of(path));
+        users.save(Path.of(path));
     }
 
     public void save(Path path) throws IOException {
-        Gson gson = new Gson();
-        Files.writeString(path, gson.toJson(users));
+        users.save(path);
     }
-
 }
