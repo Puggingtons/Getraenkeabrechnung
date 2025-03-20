a19344811b9716fe4e3d27929560180cc04eff6f
Paul Bader <paul@pbdr.de>
2025-03-20 15:19:28 +0100

    feat: add UserDatabase

---
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/data/UserDatabase.java
@@ -0,0 +1,58 @@
+package de.dhbw.karlsruhe.getraenkeabrechnung.data;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import de.dhbw.karlsruhe.getraenkeabrechnung.User;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDatabase {
+    private List<User> users;
+    private static final String PATH = "users.json";
+
+    public UserDatabase() {
+        this.users = new ArrayList<>();
+    }
+
+    public User[] getUsers() {
+        return users.toArray(new User[0]);
+    }
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    public void load() throws IOException {
+        load(PATH);
+    }
+
+    public void load(String path) throws IOException {
+        load(Path.of(path));
+    }
+
+    public void load(Path path) throws IOException {
+        Gson gson = new Gson();
+
+        // https://github.com/google/gson/blob/main/UserGuide.md#collections-examples
+        TypeToken<List<User>> typeToken = new TypeToken<List<User>>() {};
+        this.users = gson.fromJson(Files.readString(path), typeToken.getType());
+    }
+
+    public void save() throws IOException {
+        save(PATH);
+    }
+
+    public void save(String path) throws IOException {
+        save(Path.of(path));
+    }
+
+    public void save(Path path) throws IOException {
+        Gson gson = new Gson();
+        Files.writeString(path, gson.toJson(users));
+    }
+
+}
