fb32ed75e9abdd51652cba282a5083eef3ea293d
Kayra Güler <kayra.gueler32@gmail.com>
2025-04-17 13:41:31 +0200

    Added min and max range for NumberInput. Fixes #46

--- src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/io/input/NumberInput.java
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/io/input/NumberInput.java
@@ -4,16 +4,28 @@ import de.dhbw.karlsruhe.getraenkeabrechnung.io.input.result.Result;
 
 public class NumberInput extends Input<Integer> {
 
-//    todo: range? (min and max value)
+    private final int minValue;
+    private final int maxValue;
 
     public NumberInput(String prompt) {
         super(prompt);
+        this.minValue = 1;
+        this.maxValue = 99;
+    }
+
+    public NumberInput(String prompt, int minValue, int maxValue) {
+        super(prompt);
+        this.minValue = minValue;
+        this.maxValue = maxValue;
     }
 
     @Override
     Result<Integer> getResult(String input) {
         try {
             Integer res = Integer.valueOf(input);
+            if (res < minValue || res > maxValue) {
+                return Result.none();
+            }
             return Result.some(res);
         } catch (NumberFormatException e) {
             return Result.none();
