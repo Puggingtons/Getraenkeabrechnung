f1c6b03eb2198856c3a8bfa6afec6e88e0ec69bc
Fichtentee <ficht.sarah@yahoo.de>
2025-04-28 22:12:37 +0200

    feat: :sparkles: added class for specific drink (in its category)

---
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/DrinkOption.java
@@ -0,0 +1,28 @@
+package de.dhbw.karlsruhe.getraenkeabrechnung;
+
+public class DrinkOption {
+        
+    private DrinkName drinkName;
+    private ColorName colorName;
+
+    public DrinkOption(DrinkName drinkName, ColorName colorName) {
+        this.drinkName = drinkName;
+        this.colorName = colorName;
+    }
+
+    public DrinkName getDrinkName() {
+        return drinkName;
+    }
+
+    public void setDrinkName(DrinkName drinkName) {
+        this.drinkName = drinkName;
+    }
+
+    public ColorName getColorName() {
+        return colorName;
+    }
+
+    public void setColorName(ColorName colorName) {
+        this.colorName = colorName;
+    }
+}
