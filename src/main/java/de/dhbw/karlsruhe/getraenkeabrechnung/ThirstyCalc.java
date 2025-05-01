d64be5e171b608f6b3c1cc513ebc74c84da29266
Paul Bader <paulbader@gmx.de>
2025-05-01 16:32:36 +0200

    feat: add logging to thirstycalc

--- src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/ThirstyCalc.java
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/ThirstyCalc.java
@@ -4,6 +4,9 @@ import de.dhbw.karlsruhe.getraenkeabrechnung.banking.Account;
 import de.dhbw.karlsruhe.getraenkeabrechnung.data.AccountDatabase;
 import de.dhbw.karlsruhe.getraenkeabrechnung.data.DrinkDatabase;
 import de.dhbw.karlsruhe.getraenkeabrechnung.data.UserDatabase;
+import de.dhbw.karlsruhe.getraenkeabrechnung.logging.Logger;
+import de.dhbw.karlsruhe.getraenkeabrechnung.logging.LoggerFactory;
+import de.dhbw.karlsruhe.getraenkeabrechnung.logging.UserLogger;
 import de.dhbw.karlsruhe.getraenkeabrechnung.state.ApplicationState;
 
 import java.io.IOException;
@@ -15,7 +18,15 @@ public class ThirstyCalc {
 
     private final ApplicationState applicationState;
 
+    private Logger logger;
+
     public ThirstyCalc() {
+        this(new LoggerFactory().defaultTimeLogger());
+    }
+
+    public ThirstyCalc(Logger logger) {
+        this.logger = logger;
+
         userDatabase = new UserDatabase();
         accountDatabase = new AccountDatabase();
 
@@ -44,28 +55,36 @@ public class ThirstyCalc {
         if (!userDatabase.userExists(user.getUsername())) {
             // todo handle user does not exist
             System.out.println("user does not exist");
+            logger.log("failed login attempt for user " + user.getUsername() + " because user does not exist");
             return;
         }
 
         if (applicationState.isLoggedIn()) {
             // todo handle user is already logged in
             System.out.println("user already logged in");
+            logger.log("failed login attempt for user " + user.getUsername() + " because there is already a logged in user");
             return;
         }
 
         applicationState.setLoggedInUser(user);
+
+        this.logger = new UserLogger(user, this.logger);
     }
 
     public void logout() {
+        logger.log("user " + applicationState.getLoggedInUser().getUsername() + " logged out");
         applicationState.clearLoggedInUser();
+        this.logger = this.logger.getInnerLogger();
     }
 
     public void createNewUser(User user) {
+        logger.log("creating new user " + user.getUsername());
         userDatabase.registerNewUser(user);
         accountDatabase.createAccount(user);
     }
 
     public void createNewDrinkOption(DrinkOption drinkOption) {
+        logger.log("creating new drink option " + drinkOption.getDrinkName() + " with color " + drinkOption.getColorName());
         drinkDatabase.createNewDrinkOption(drinkOption);
         System.out.println("Creating a new drink option: " + drinkOption);
     }
@@ -76,6 +95,7 @@ public class ThirstyCalc {
     
 
     public void deleteUser(User user) {
+        logger.log("deleting user " + user.getUsername());
         userDatabase.deleteUser(user);
         accountDatabase.removeAccount(user);
     }
@@ -98,15 +118,21 @@ public class ThirstyCalc {
 
     public void save() {
         try {
+            logger.log("saving databases");
+
+            logger.log("saving users.json");
             System.out.println("Saving users.json");
             userDatabase.save("users.json");
 
+            logger.log("saving accounts.json");
             System.out.println("Saving accounts.json");
             accountDatabase.save("accounts.json");
 
+            logger.log("saving drinks.json");
             System.out.println("Saving drinks.json");
             drinkDatabase.save("drinks.json");
 
+            logger.log("finished saving databases");
         } catch (IOException e) {
             System.out.println("Could not save users");
         }
@@ -114,9 +140,18 @@ public class ThirstyCalc {
 
     public void load() {
         try {
+            logger.log("loading databases");
+
+            logger.log("loading users.json");
             userDatabase.load("users.json");
+
+            logger.log("loading accounts.json");
             accountDatabase.load("accounts.json");
+
+            logger.log("loading drinks.json");
             drinkDatabase.load("drinks.json");
+
+            logger.log("finished loading databases");
 
         } catch (IOException e) {
             System.out.println("Could not load users, accounts or drinks!");
