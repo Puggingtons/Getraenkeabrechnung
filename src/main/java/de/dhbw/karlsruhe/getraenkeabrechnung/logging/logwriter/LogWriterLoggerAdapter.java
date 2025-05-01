1d984f9b63e32f2a4f2545962863ff7a56c940c9
Paul Bader <paulbader@gmx.de>
2025-05-01 14:54:52 +0200

    feat: add logwriter to logger adapter

---
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/logging/logwriter/LogWriterLoggerAdapter.java
@@ -0,0 +1,17 @@
+package de.dhbw.karlsruhe.getraenkeabrechnung.logging.logwriter;
+
+import de.dhbw.karlsruhe.getraenkeabrechnung.logging.Logger;
+
+public class LogWriterLoggerAdapter implements Logger {
+
+    private final LogWriter logWriter;
+
+    public LogWriterLoggerAdapter(LogWriter logWriter) {
+        this.logWriter = logWriter;
+    }
+
+    @Override
+    public void log(String message) {
+        this.logWriter.write(message);
+    }
+}
