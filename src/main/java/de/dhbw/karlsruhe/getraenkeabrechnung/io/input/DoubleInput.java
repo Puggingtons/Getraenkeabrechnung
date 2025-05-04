d3c9de49d67ffd6197d9ff9ce2015e09bccfb474
Fichtentee <ficht.sarah@yahoo.de>
2025-05-04 13:55:17 +0200

    feat: :sparkles: added couble input for interacction to add price for category

---
+++ src/main/java/de/dhbw/karlsruhe/getraenkeabrechnung/io/input/DoubleInput.java
@@ -0,0 +1,20 @@
+package de.dhbw.karlsruhe.getraenkeabrechnung.io.input;
+
+import de.dhbw.karlsruhe.getraenkeabrechnung.io.input.result.Result;
+
+public class DoubleInput extends Input<Double> {
+
+    public DoubleInput(String prompt) {
+        super(prompt);
+    }
+
+    @Override
+    Result<Double> getResult(String input) {
+        try {
+            return Result.some(Double.parseDouble(input));
+        } catch (NumberFormatException e) {
+            return Result.none("Invalid number format");
+        }
+    }
+    
+}
